46a24b6e7053128529dd60bc86c80705215fe446
tungyr-one <tungusov.victor@gmail.com>
2023-03-11 19:58:30 +0300

    End of section 14

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr';
+import { MembersService } from 'src/app/_services/members.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 
@@ -11,9 +13,15 @@ export class MemberCardComponent implements OnInit {
   @Input() member:Member;
   // member:Member;
 
-  constructor() { }
+  constructor(private memberService:MembersService, private toastr:ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  addLike(member:Member){
+    this.memberService.addLike(member.username).subscribe({
+      next:()=> this.toastr.success('You have liked ' + member.knownAs)
+    })
   }
 
 }
